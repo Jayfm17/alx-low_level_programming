f64528076748c65cc32976d06dfd64bf070cce10
jayfm17 <jayfm17@gmail.com>
2023-03-17 05:32:25 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,17 @@
  * Author: Jefferson M
  * Purpose: print whether number is positive or negative
  * Language: C language
+ * return: 0(positive)
  */
 int main(void)
 {
 	int n;
-		if(n==0){
+		if(n==0)
+		{
 			printf("positive\n");
 		}
-		else if(n<0){
+		else if(n<0)
+		{
 			printf("negative\n");
 		}
 		return 0;
