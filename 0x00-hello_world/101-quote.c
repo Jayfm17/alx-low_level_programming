3946791875cf5bebd71863bd9e07ff75d631abde
jayfm17 <jayfm17@gmail.com>
2023-03-19 07:03:47 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Description: program that prints new line without printf or puts
+ * Description: Program that prints line in standard output without printf of puts
  * parameters: fput
  *
- * Return: Always 1 (success)
+ * return: Always 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
-	
-
