f54118df49d24d077598716e3fcfea58c3ac5f1f
jayfm17 <jayfm17@gmail.com>
2023-03-19 04:29:40 -0700

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints size of various types
+ * parameters: gcc
+ *
+ * Return: Always 0 (successs)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
