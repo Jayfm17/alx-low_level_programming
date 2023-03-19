4114fe60298aba6ec202c9c93fa5f88fc384d457
jayfm17 <jayfm17@gmail.com>
2023-03-19 06:35:59 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints new line without printf or puts
+ * parameters: fput
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+	return (1);
+}
+	
+
