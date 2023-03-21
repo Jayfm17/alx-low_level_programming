de4575e4e15dfe8994eddf86f1557435e50266dd
jayfm17 <jayfm17@gmail.com>
2023-03-21 13:21:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Dscription: Program that prints _putchar followed by new line
+ * parameters: putchar
+ *
+ * return: 0(success)
+ */
+
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}
