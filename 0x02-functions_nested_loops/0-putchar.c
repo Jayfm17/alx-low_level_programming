c7b8c2862ab5838e312418282d69d3440f9730f3
jayfm17 <jayfm17@gmail.com>
2023-03-21 14:13:57 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
-#include <stdio>
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 /**
  * main - Entry point
  *
  * Dscription: Program that prints _putchar followed by new line
  * parameters: putchar
  *
- * return: 0(success)
+ * return: 0 reads success else error
  */
 
-int main(void)
-{
-	putchar("_putchar\n");
+int _putchar (char c);
+(
 	return (0);
 }
