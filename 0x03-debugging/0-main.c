631b185d08b1ae12b10a2c2b51ed7225909a68be
jayfm17 <jayfm17@gmail.com>
2023-03-23 13:16:14 -0700

    positive_or_negative debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - Entry point
+ * main - Entry point to test for positive_or_negative()
  *
  * Description: Testing debugging on positive and negative file
  * parameters: if and else function
  *
  * return: Always 0 (success)
  */
+
 int main(void)
 {
 	int i;
+
 	i = 98;
 	positive_or_negative(i);
 	return (0);
