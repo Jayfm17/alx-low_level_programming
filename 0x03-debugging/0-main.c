1568c171410cfc7aad7cc458ff1efe737bb8a1b9
jayfm17 <jayfm17@gmail.com>
2023-03-23 14:09:27 -0700

    positive_or_negative debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
 * main - tests function that prints if integer is positive or negative.
+*
+* Description: Gives correct output when given a case of 0.
+* parameters: To upload 0-main.c and main.h
+*
 * Return: 0
 */
 
