ab7ae83a1889166c814d4e4931c72485300ce6d8
jayfm17 <jayfm17@gmail.com>
2023-03-23 13:07:22 -0700

    Debugging 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Testing debugging on positive and negative file
+ * parameters: if and else function
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	i = 98;
+	positive_or_negative(i);
+	return (0);
+}
